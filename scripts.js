196f68ff3ed758aea6f80a6eb6bbb99f8120775f
Rajesh112k <60816939+Rajesh112k@users.noreply.github.com>
2024-06-05 01:57:03 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function showTab(event, tabId) {
+    var tabContents = document.getElementsByClassName('tab-content');
+    for (var i = 0; i < tabContents.length; i++) {
+        tabContents[i].classList.remove('active');
+    }
+    document.getElementById(tabId).classList.add('active');
+    
+    var tabButtons = document.getElementsByClassName('tab-button');
+    for (var i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].classList.remove('active');
+    }
+    event.currentTarget.classList.add('active');
+}
